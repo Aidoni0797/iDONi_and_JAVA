e7e7398bb9f284a0f29923baed0cf5d9302ac9f2
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-15 23:27:01 +0600

    salem sozdin anasy

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    public static void main(String[] args) {
+//        Это аналог если сравнить с go fmt.Println , прикольно, просто изучай синтаксиси языка, iDONi, молодец
+        System.out.println("Hello, iDONi!");
+    }
+}
