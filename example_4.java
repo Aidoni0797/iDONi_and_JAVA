b092737392e00976c050d4f71d140cddc590b170
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 00:09:22 +0600

    example_4

---
+++ example_4.java
@@ -0,0 +1,11 @@
+public class example_4 {
+    static int x = 15;
+    static int y = 22;
+    public static void main(String args[]) {
+        int x = 12;
+        System.out.println(x);//обращение к локальной
+        System.out.println(y); //обращение к полю класса
+        System.out.println(z); //обращение к полю класса
+    }
+    static int z = 25;
+}
