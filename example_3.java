65a662e2eb48b487a987a7e540c67a93032007b9
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 00:08:29 +0600

    example_3

---
+++ example_3.java
@@ -0,0 +1,9 @@
+public class example_3 {
+    public static void main(String args[]){
+        int x=12;
+        {
+            // int x=18; //ошибка
+            System.out.println(x);
+        }
+    }
+}
