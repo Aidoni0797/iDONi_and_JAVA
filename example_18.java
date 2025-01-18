a5fe7dad09aa376b388ef28d1ba0e7a5a0121e70
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 15:03:26 +0600

    example_18

---
+++ example_18.java
@@ -0,0 +1,8 @@
+public class example_18 {
+    public static void main(String[] args) {
+        double x = 3.3;
+        int k = 3 / 2 + x * 2;
+        System.out.println(k);
+    }
+}
+// Код не скомпилируется
