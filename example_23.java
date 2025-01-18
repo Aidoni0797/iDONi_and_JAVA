09670277c96df72dd83a86e82355c2c0bccff256
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 16:40:00 +0600

    example_23

---
+++ example_23.java
@@ -0,0 +1,8 @@
+public class example_23 {
+    public static void main(String[] args) {
+        double x = 3.123456;
+        System.out.printf("x=%8.2f\n", x);//число выводится в 8 позициях
+        //и округляется до 2 знаков после запятой
+    }
+}
+//очень длинный получился промежуток, прикольно
