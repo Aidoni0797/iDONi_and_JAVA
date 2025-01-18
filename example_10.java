b08d6ed4372d4d80cd0c0c3db550e9ed65bf13d7
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 13:41:04 +0600

    example_10

---
+++ example_10.java
@@ -0,0 +1,8 @@
+public class example_10 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        System.out.println(a && (b || !c));
+    }
+}
