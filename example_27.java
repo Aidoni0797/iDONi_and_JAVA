114d27d1737b64de786c1404621b997c03db9af5
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 18:44:13 +0600

    example_27

---
+++ example_27.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class example_27 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a,b;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a+b);
+    }
+}
