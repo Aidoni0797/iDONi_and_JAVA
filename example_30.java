f7ee5bc191779aced55b498cd7f2ecc9c2c0b132
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 19:31:57 +0600

    example_30

---
+++ example_30.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class example_30 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+
+        System.out.println(number % 10);
+        System.out.println(number / 10);
+    }
+}
+//заяем я это делаю но делаю
