09ade69812f6495f38ec878fb132ec0dc2dc6e6d
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 14:23:00 +0600

    example_16

---
+++ example_16.java
@@ -0,0 +1,8 @@
+public class example_16 {
+    public static void main(String[] args) {
+        char ch1 = 'a', ch2 = 'b';
+        ch1 = (char)(ch1 + ch2);
+        System.out.println(ch1);
+    }
+}
+//очень интересно и неожиданно
