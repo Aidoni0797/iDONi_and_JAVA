4c0751381421d122d7c35957ed3742883025a2e6
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 15:04:56 +0600

    example_19

---
+++ example_19.java
@@ -0,0 +1,9 @@
+public class example_19 {
+    public static void main(String[] args) {
+        int k = 4;
+        double x = 3.14;
+        short s = -5;
+        System.out.print((int)x + k * s);
+        System.out.println(" " + (int)(x + k * s));
+    }
+}
