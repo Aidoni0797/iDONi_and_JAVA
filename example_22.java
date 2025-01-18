640fcc919cca4a41ec3529cd2b814910a6161928
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 16:33:34 +0600

    example_22

---
+++ example_22.java
@@ -0,0 +1,6 @@
+public class example_22 {
+    public static void main(String[] args) {
+        int rub = 3,kop = 0;
+        System.out.printf("%02d руб. %02d коп.", rub, kop);//выводится: 03 руб. 00 коп.
+    }
+}
