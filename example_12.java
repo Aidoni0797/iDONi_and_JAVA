739dd47a4e85a4030cf06738bacc3eebbc640747
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 13:44:53 +0600

    example_12

---
+++ example_12.java
@@ -0,0 +1,9 @@
+public class example_12 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        int c = (a > b) ? a : b;
+        c += 2;
+        System.out.println(c);
+    }
+}
