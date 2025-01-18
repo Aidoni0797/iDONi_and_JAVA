45131e3fc403f67f5be59b281590df178aaf8811
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 13:23:03 +0600

    example_5

---
+++ example_5.java
@@ -0,0 +1,9 @@
+public class example_5 {
+    public static void main(String[] args) {
+        int number = 3;
+        System.out.println(++number);  //выводится 4, значение number равно 4
+
+        int number2 = 3;
+        System.out.println(number2++); //выводится 3, значение number2 равно 4
+    }
+}
