5954711bd87cf2c8caa7570ea241ebf119691a8a
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-18 16:23:12 +0600

    example_21

---
+++ example_21.java
@@ -0,0 +1,13 @@
+public class example_21 {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.println(", World!"); //вывод Привет, мир!
+        System.out.println("iDONi" + " is " + "thinking");//вывод: Мама мыла раму
+        int i = 1;
+        int j = 2;
+        System.out.println("i=" + i); //вывод i=7
+        System.out.println("i+j=" + i + j); //вывод i+j=75
+        System.out.println("i+j=" + (i + j)); //вывод i+j=12
+        System.out.println(i + j); //вывод 12
+    }
+}
