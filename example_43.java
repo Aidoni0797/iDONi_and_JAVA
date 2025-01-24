b05df2f21b909aecc2f97586296e64786cce5b26
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-25 00:35:12 +0600

    example_43

---
+++ example_43.java
@@ -0,0 +1,13 @@
+public class example_43 {
+    public static void main(String[] args) {
+        double three = 3.0;
+        double x = 1 / three;
+        double y = 4 / three;
+        double z = 5 / three;
+        if (x + y == z) {
+            System.out.println("1/3 + 4/3 = 5/3");
+        } else {
+            System.out.println("1/3 + 4/3 != 5/3");
+        }
+    }
+}
