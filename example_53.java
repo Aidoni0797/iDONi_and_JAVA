aa94070605410bd41e4b6e9bd262bc31e1906721
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-25 19:16:09 +0600

    example_53

---
+++ example_53.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class example_53 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        System.out.println(max);
+    }
+}
