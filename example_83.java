e04c527e5370c2adb7e0c839cd87b5e2e4f4f274
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-02-01 23:56:23 +0600

    example_83

---
+++ example_83.java
@@ -0,0 +1,9 @@
+public class example_83 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int result = a + b;
+        System.out.println("Result: " + result);
+    }
+}
+//iDONi когда ты поймешь работать с отладками наступить этот день или нет
