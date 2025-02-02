dda5047cecbcd80b4e48d9d0ee48a9ca6b8edb8e
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-02-02 18:33:30 +0600

    example_89

--- example_89.java
+++ example_89.java
@@ -7,15 +7,34 @@ public class example_89 {
         if (a<=0){
             System.out.print("ERROR");
         }else{
-            for(int i = 1; i <= a; i++) {
-                for(int j = 1; j <= a; j++) {
-                    if (j>i || j==i){
-                        System.out.print("*");
-                    } else{
-                        System.out.print(" ");
+            if (a%2!=0){
+                for(int i = 1; i <= a; i++) {
+                    for(int j = 1; j <= a/2; j++) {
+                        if (j>i || j==i){
+                            System.out.print("**");
+                        } else{
+                            System.out.print(" ");
+                        }
                     }
+                    if (((a/2)+1) == i){
+                        System.out.print("*");
+                        break;
+                    } else{
+                        System.out.print("*");
+                    }
+                    System.out.println();
                 }
-                System.out.println();
+            } else{
+                for(int i = 1; i <= a; i++) {
+                    for(int j = 1; j <= a/2; j++) {
+                        if (j>i || j==i){
+                            System.out.print("**");
+                        } else{
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
+                }
             }
         }
     }
