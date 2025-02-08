582ae5ed9bbbd93c6893e511497aceb63b33b306
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-02-08 13:38:39 +0600

    example_106

---
+++ example_106.java
@@ -0,0 +1,15 @@
+public class example_106 {  //Tests - имя класса
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        example_106 data = new example_106();  //создание экуземпляра класса Tests
+        //вызов метода
+    }
+    static void plus(int x, int y){
+        System.out.println(x + " + " + y);
+    }
+    void minus(int x, int y){
+        System.out.println(x + " - " + y);
+    }
+}
