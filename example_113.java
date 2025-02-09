d1269a41352c1e83167a17ba5dadc626fa1831fc
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-02-09 11:37:29 +0600

    example_113

---
+++ example_113.java
@@ -0,0 +1,13 @@
+public class example_113 {
+    public static void main(String[] args) {
+        double a = 5;
+        double b = 6;
+        System.out.println(fun(a, b));
+    }
+    static int fun(int x, int y){
+        return x + y;
+    }
+    static double fun(double x, double y){
+        return x * y ;
+    }
+}
